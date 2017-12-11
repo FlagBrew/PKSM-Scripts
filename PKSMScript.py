2e3dff896577bb30be505e7108ec3e268cdefd57
bernardogiordano <bernardogiordano369@gmail.com>
2017-12-11 15:09:36 +0100

    add scripting resources

---
+++ PKSMScript.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import argparse
+
+
+# PKSM script structure
+# u8 magic[10]        // PKSMSCRIPT
+# u32 offset          // save offset to write the data to
+# u32 length          // payload length
+# u8 payload[len]     // payload data
+# u32 repeat_times    // repeat payload n times
+# ...
+ 
+ 
+parser = argparse.ArgumentParser(description = 'Creates .pksm script files')
+parser.add_argument('output', help = 'Output file name')
+parser.add_argument('-i', action = 'append', nargs = 4, metavar = ('ofs', 'len', 'pld', 'rpt'), help = 'ofs: offset to write the payload to. len: payload length. pld: payload (can be an integer or a file path), rpt: repeat n times.')
+
+
+def main(args):
+	script = b"PKSMSCRIPT"
+
+	for arg in args.i:
+		argoffset  = arg[0]
+		arglength  = arg[1]
+		argpayload = arg[2]
+		argrepeat  = arg[3]
+		
+		offset = int(argoffset, 0)
+		length = int(arglength, 0)
+		
+		try:
+			int (argpayload, 0)
+			payload = int(argpayload, 0).to_bytes(length, byteorder = 'little')
+		except ValueError as e:
+			with open(argpayload, 'rb') as f:
+				payload = f.read(length)		
+		
+		repeat = int(argrepeat, 0)
+
+		script += offset.to_bytes(0x4, byteorder = 'little')
+		script += length.to_bytes(0x4, byteorder = 'little')
+		script += payload
+		script += repeat.to_bytes(0x4, byteorder = 'little')
+	
+		with open(args.output + '.pksm', 'wb') as f:
+			f.write(script)
+
+	
+if __name__ == '__main__':
+	main(parser.parse_args())
