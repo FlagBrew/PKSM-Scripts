7e4b06291b39bc8ef092e8585d117375e8ef284e
Gudf <vietor.paul@yahoo.fr>
2019-02-10 21:55:59 +0100

    Fix mega evolution unlocking script
    Fix mega evolution unlocking script

---
+++ src/universal/SMUSUM - Toggle Mega Evolution.c
@@ -0,0 +1,53 @@
+#include <pksm.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+    unsigned char* saveData = (unsigned char*) atoi(argv[0]);
+    unsigned char version = *argv[2];
+    int offset;
+
+    switch (version)
+    {
+        case 30:
+        case 31:
+            offset = 0x1278;
+            break;
+        case 32:
+        case 33:
+            offset = 0x1478;
+            break;
+        default:
+            gui_warn("Mega evolution unlock not", "supported for this game");
+            return 1;
+    }
+
+    if ((saveData[offset] & 0x01) == 0)
+    {
+        if (gui_choice("Mega evolution is locked.", "Unlock it?"))
+        {
+            saveData[offset] = (saveData[offset] ^ 0x01);
+            gui_warn("Mega evolution successfully unlocked!", "");
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        if (gui_choice("Mega evolution is unlocked.", "Lock it?"))
+        {
+            saveData[offset] = (saveData[offset] ^ 0x01);
+            gui_warn("Mega evolution successfully locked!", "");
+        }
+        else 
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
