8d88b2b4a537ad62d960e3de934622dc5e3994c8
SpiredMoth <bcgame42@hotmail.com>
2019-04-18 00:07:39 -0700

    OOP rewrite: Python

--- dev/python/diffEvent.py
+++ dev/python/diffEvent.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import argparse
+import math
 import os
 
-import Sav
+from Sav import Sav
 
 # argparse setup
 parser = argparse.ArgumentParser(
@@ -16,62 +17,61 @@ group.add_argument('-s', '--saves', nargs='+',
                    help='Space separated list of save files to diff')
 
 # Global variables
-split = False
 diffs = 0
 cli = __name__ == '__main__'
 
 
 def _getVersionEventInfo(version):
-    if version == 10 or version == 11:
+    if version == 'DP':
         return {
             'constStart': 0xd9c,
             'constCount': 288,
             'flagStart': 0xfdc,
             'flagCount': 2912
         }
-    elif version == 12:
+    elif version == 'PT':
         return {
             'constStart': 0xdac,
             'constCount': 288,
             'flagStart': 0xfec,
             'flagCount': 2912
         }
-    elif version == 7 or version == 8:
+    elif version == 'HGSS':
         return {
             'constStart': 0xde4,
             'constCount': 368,
             'flagStart': 0x10c4,
             'flagCount': 2912
         }
-    elif version == 20 or version == 21:
+    elif version == 'BW':
         return {
             'constStart': 0x20100,
             'constCount': 318,
             'flagStart': 0x2037c,
             'flagCount': 2912
         }
-    elif version == 22 or version == 23:
+    elif version == 'B2W2':
         return {
             'constStart': 0x1ff00,
             'constCount': 431,
             'flagStart': 0x2025e,
             'flagCount': 3064
         }
-    elif version in (24, 25, 26, 27):
+    elif version in ('XY', 'ORAS'):
         return {
             'constStart': 0x14a00,
             'constCount': 382,
             'flagStart': 0x14cfc,
             'flagCount': 3072
         }
-    elif version == 30 or version == 31:
+    elif version == 'SM':
         return {
             'constStart': 0x1c00,
             'constCount': 1000,
             'flagStart': 0x23d0,
             'flagCount': 3968
         }
-    elif version == 32 or version == 33:
+    elif version == 'USUM':
         return {
             'constStart': 0x1e00,
             'constCount': 1000,
@@ -153,40 +153,44 @@ def _diffEventConst(eConstData):
     if len(constDiffs) > 0:
         eConstHeader = ['\nEvent Const Diff\n\nConst']
         for i in range(len(eConstData)):
-            eConstHeader.append("    Save %s" % (i + 1))
+            digStr = "    Save %d" % (i + 1)
+            digits = 10
+            if i % 10 == 9:
+                digits += (math.floor(math.log10(i + 1)) != math.floor(math.log10(i)))
+            eConstHeader.append(digStr[-digits:])
         eConstHeader.append('\n')
         constDiffs.insert(0, ''.join(eConstHeader))
         return ''.join(constDiffs)
     return '\nEvent constants are identical.\n'
 
 
-def generateDiff(files, data):
-    diff = []
-    global diffs
-
-    version = Sav.getVersion(len(data[0]), data[0])
-    if version in (-1, 42, 43):
-        if version != -1:
-            print('Event Diff does not work on LGPE yet.')
+def generateDiff(saves):
+    if saves[0].verGroupAbbr == 'LGPE':
+        print('Event Diff does not work on LGPE yet.')
         return ''
-    versionInfo = _getVersionEventInfo(version)
+    versionInfo = _getVersionEventInfo(saves[0].verGroupAbbr)
 
     flagData = []
     constData = []
-    for i in range(len(data)):
+    for i in range(len(saves)):
+        raw = saves[i].getCurrent()
+
         # extract Event Flags
-        rawFlag = data[i][versionInfo['flagStart']:versionInfo['flagStart'] + (versionInfo['flagCount'] >> 3)]
-        resultData = []
+        data = raw[versionInfo['flagStart']:versionInfo['flagStart'] + (versionInfo['flagCount'] >> 3)]
+        extracted = []
         for n in range(versionInfo['flagCount']):
-            resultData.append((rawFlag[n >> 3] >> (n & 7) & 1) == 1)
-        flagData.append(resultData)
+            extracted.append((data[n >> 3] >> (n & 7) & 1) == 1)
+        flagData.append(extracted[:])
 
         # extract Event Constants
-        rawConst = data[i][versionInfo['constStart']:versionInfo['flagStart']]
-        resultData = []
+        data = raw[versionInfo['constStart']:versionInfo['flagStart']]
+        extracted = []
         for n in range(versionInfo['constCount']):
-            resultData.append(rawConst[n * 2] + (rawConst[n * 2 + 1] << 8))
-        constData.append(resultData[:])
+            extracted.append(data[n * 2] + (data[n * 2 + 1] << 8))
+        constData.append(extracted[:])
+
+    diff = []
+    global diffs
 
     # diff event data
     flagDiffs = _diffEventFlags(flagData)
@@ -204,45 +208,38 @@ def generateDiff(files, data):
         # if not called from another script, construct diff header
         if cli:
             header = ['Diff of saves from the following file(s):\n\n']
-            for i in range(len(files)):
-                header.append("Save %i: %s\n" % (i + 1, files[i]))
-            diff.append("\n")
+            for i in range(len(saves)):
+                contents = '' if saves[i].contents == 'full' else ' (%s)' % saves[i].contents
+                header.append("Save %i: %s%s\n" % (i + 1, saves[i].fileName, contents))
+            header.append("\n")
             diff.insert(0, ''.join(header))
         return ''.join(diff)
     return 'Event flags and constants of provided saves are identical.'
 
 
 def main(params):
-    saveFiles = params.saves
-
     print("Loading saves...")
-    fileBuffs = Sav.loadSaves(saveFiles)
-    failedFiles = 0
-    for i in range(len(fileBuffs)):
-        if fileBuffs[i] == None:
-            del fileBuffs[i - failedFiles]
-            del saveFiles[i - failedFiles]
-            failedFiles += 1
+    saves = [Sav(f) for f in params.saves]
+    saves = [s for s in saves if s.version != -1]
 
     print("Checking compatibility of saves...")
-    compatFailReason = Sav.checkCompat(fileBuffs)
+    compatFailReason = Sav.checkCompat(saves)
     if compatFailReason != '':
         print("Save compatibility check failed.\nReason: \"%s\"\nExiting without diffing." %
               compatFailReason)
         return
 
     # check number of saves
-    if len(fileBuffs) == 1:
-        split = Sav.canSplit(fileBuffs[0])
-        if not split:
+    if len(saves) == 1:
+        if not saves[0].canSplit():
             print(
-                "Cannot split a single save file from this game. Exiting without diffing.")
+                "Cannot split a single %s save file. Exiting without diffing." % saves[0].verGroupAbbr)
             return
         # split lone save
-        Sav.splitSave(saveFiles, fileBuffs)
+        Sav.splitSave(saves, 0, True)
 
     print("Generating diff...")
-    diff = generateDiff(saveFiles, fileBuffs)
+    diff = generateDiff(saves)
 
     # output diff
     if diffs == 0:
@@ -271,7 +268,11 @@ if cli:
         if params.saves == None:
             params.saves = []
         for d in range(len(params.dir)):
+            dirFiles = []
             for path, _, files in os.walk(params.dir[d]):
                 for fullname in files:
-                    params.saves.append(os.path.normpath(os.path.join(path, fullname)))
+                    dirFiles.append(os.path.normpath(
+                        os.path.join(path, fullname)))
+            dirFiles.sort(key=str.lower)
+            params.saves.extend(dirFiles)
     main(params)
--- dev/python/diffSave.py
+++ dev/python/diffSave.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import math
 import argparse
 import os
 
-import Sav
+from Sav import Sav
 from diffEvent import generateDiff as diffEvent
 
 # argparse setup
@@ -12,16 +13,23 @@ parser.add_argument('-o', metavar='outFile',
                     help='File (.txt) to write diff to -- if not given, diff is written to console')
 parser.add_argument('-r', '--range', nargs=2, metavar=('start', 'end'),
                     help='Specific range to limit diff to, from start (included) to end (excluded)')
-parser.add_argument('-e', action='store_true', help='Flag to include an event diff')
+# Length-based range diffing
+# parser.add_argument('-l', action='append', nargs=2, metavar=('start', 'length'),
+#                     help='Specific range to limit diff to, length number of bytes beginning at start')
+# Diff full file rather than just current save data (of split-able save file types)
+# parser.add_argument('-f', action='store_true',
+#                     help='Use full save file (of saves that can be split) in diff rather than just current save data')
+parser.add_argument('-e', action='store_true',
+                    help='Flag to include an event diff')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-d', '--dir', metavar='dir', nargs='+',
-                    help='Directory (folder) containing files to diff')
+                   help='Directory (folder) containing files to diff')
 group.add_argument('-s', '--saves', nargs='+',
-                    help='Space separated list of save files to diff')
+                   help='Space separated list of save files to diff')
 
 # Global variables
-split = False
 diffs = 0
+
 
 def _toPaddedHexString(value, padTo):
     paddedHex = hex(value)[2:]
@@ -30,50 +38,99 @@ def _toPaddedHexString(value, padTo):
     return "0x" + paddedHex[-padTo:]
 
 
-def generateDiff(files, data, diffRange, eventDiff):
+def generateDiff(saves, config):
     diff = []
     global diffs
+
+    ofs = {
+        'general': [0],
+        'storage': [0],
+        'hof': [0],
+        'save': [0],
+        'extra': [0]
+    }
+    if saves[0].generation == 4:
+        ofs['general'] = Sav.getBlockOffsets(saves[0].verGroupAbbr, 'general')
+        ofs['storage'] = Sav.getBlockOffsets(saves[0].verGroupAbbr, 'storage')
+        ofs['hof'] = Sav.getBlockOffsets(saves[0].verGroupAbbr, 'hof')
+    elif saves[0].generation == 5:
+        ofs['save'] = Sav.getBlockOffsets(saves[0].verGroupAbbr, 'save')
+        ofs['extra'] = Sav.getBlockOffsets(saves[0].verGroupAbbr, 'extra')
 
     # construct diff header
     tableHeader = ["\nOffset   "]
     diff.append("Diff of saves from the following file(s):\n\n")
-    for i in range(len(files)):
-        diff.append("Save %d: %s\n" % (i + 1, files[i]))
-        tableHeader.append("    Save %d" % (i + 1))
+    for i in range(len(saves)):
+        contents = '' if saves[i].contents == 'full' else ' (%s)' % saves[i].contents
+        diff.append("Save %d: %s%s\n" % (i + 1, saves[i].fileName, contents))
 
-    if len(data[0]) == 0x40000:
-        diff.append("\nNote: Due to the way Gen 4 saves are stored, data in Save 1 may be more recent than the corresponding data in Save 2")
+        if config['isSplit']:
+            if saves[i].generation == 4:
+                compOfs = [_toPaddedHexString(n + saves[i].active['general'] * 0x40000, 5) for n in ofs['general']]
+                diff.append("    General: %s\n" % ':'.join(compOfs))
+                compOfs = [_toPaddedHexString(n + saves[i].active['storage'] * 0x40000, 5) for n in ofs['storage']]
+                diff.append("    Storage: %s\n" % ':'.join(compOfs))
+                compOfs = [_toPaddedHexString(n + saves[i].active['hof'] * 0x40000, 5) for n in ofs['hof']]
+                diff.append("    HoF:     %s\n" % ':'.join(compOfs))
+            elif saves[i].verGroupAbbr == 'BW':
+                compOfs = [_toPaddedHexString(n + saves[i].active['save'] * 0x24000, 5) for n in ofs['save']]
+                diff.append("    Save:  %s\n" % ':'.join(compOfs))
+                compOfs = [_toPaddedHexString(n + saves[i].active['extra'] * 0x24000, 5) for n in ofs['extra']]
+                diff.append("    Extra: %s\n" % ':'.join(compOfs))
+            elif saves[i].verGroupAbbr == 'B2W2':
+                compOfs = [_toPaddedHexString(n + saves[i].active['save'] * 0x26000, 5) for n in ofs['save']]
+                diff.append("    Save:  %s\n" % ':'.join(compOfs))
+                compOfs = [_toPaddedHexString(n + saves[i].active['extra'] * 0x26000, 5) for n in ofs['extra']]
+                diff.append("    Extra: %s\n" % ':'.join(compOfs))
+            else:
+                print('How did you split this input?')
+
+        digStr = "    Save %d" % (i + 1)
+        digits = 10
+        if i % 10 == 9:
+            digits += (math.floor(math.log10(i + 1)) != math.floor(math.log10(i)))
+        tableHeader.append(digStr[-digits:])
+
+    if saves[0].generation == 4 and config['isSplit']:
+        diff.append(
+            "\nNote: HoF block of Save 1 may be more recent than the HoF block of Save 2\n")
 
     # event diffing (function call?) goes here
-    if eventDiff:
-        eDiff = diffEvent(files, data)
+    if config['event']:
+        eDiff = diffEvent(saves)
         if eDiff == '':
             print('Event diff failed.')
         else:
-            diffs += 0 if eDiff[0] == 'E' else 1
+            diffs += 0 if eDiff[:25] == 'Event flags and constants' else 1
             diff.append("\n%s" % eDiff)
 
     # range slicing
-    saveData = data
+    slicedData = [saves[i].data for i in range(len(saves))]
     sliceStart = 0
-    if diffRange != None:
-        sliceStart = int(diffRange[0], 0)
-        for i in range(len(data)):
-            saveData[i] = data[i][sliceStart:int(diffRange[1], 0)]
-        diff.append("\n\nDiff range: %s-%s" % (diffRange[0], diffRange[1]))
+    if config['range'] != None:
+        sliceStart = int(config['range'][0], 0)
+        for i in range(len(slicedData)):
+            slicedData[i] = slicedData[i][sliceStart:int(config['range'][1], 0)]
+        diff.append("\n\nDiff range: %s-%s" % (config['range'][0], config['range'][1]))
 
     # construct diff body
     diff.append("\n\n%s\n" % ''.join(tableHeader))
-    for i in range(len(saveData[0])):
+    for n in range(len(slicedData[0])):
         offsetDiff = {
-            'offset': _toPaddedHexString(i + sliceStart, 5),
+            'offset': _toPaddedHexString(n + sliceStart, 5),
             'values': []
         }
-        offsetDiff['values'] = [_toPaddedHexString(data[n][i], 2) for n in range(len(data))]
-        for n in range(len(offsetDiff['values'])):
-            if offsetDiff['values'][n] != offsetDiff['values'][0]:
+        if config['isSplit'] and saves[0].generation == 5:
+            if n > ofs['save'][1]:
+                offsetDiff['offset'] = _toPaddedHexString(n + sliceStart + ofs['save'][1] + 1, 2)
+            pass
+        offsetDiff['values'] = [_toPaddedHexString(
+            slicedData[i][n], 2) for i in range(len(slicedData))]
+        for i in range(len(offsetDiff['values'])):
+            if offsetDiff['values'][i] != offsetDiff['values'][0]:
                 diffs += 1
-                diff.append("%s      %s\n" % (offsetDiff['offset'], '      '.join(offsetDiff['values'])))
+                diff.append("%s      %s\n" % (
+                    offsetDiff['offset'], '      '.join(offsetDiff['values'])))
                 break
 
     # join diff pieces
@@ -81,37 +138,40 @@ def generateDiff(files, data, diffRange, eventDiff):
 
 
 def main(params):
-    saveFiles = params.saves
+    config = {
+        'range': params.range,
+        'event': params.e,
+        'isSplit': False
+    }
 
     print("Loading saves...")
-    fileBuffs = Sav.loadSaves(saveFiles)
-    failedFiles = 0
-    for i in range(len(fileBuffs)):
-        if fileBuffs[i] == None:
-            del fileBuffs[i - failedFiles]
-            del saveFiles[i - failedFiles]
-            failedFiles += 1
+    saves = [Sav(f) for f in params.saves]
+    saves = [s for s in saves if s.version != -1]
 
     print("Checking compatibility of saves...")
-    compatFailReason = Sav.checkCompat(fileBuffs)
+    compatFailReason = Sav.checkCompat(saves)
     if compatFailReason != '':
-        print("Save compatibility check failed.\nReason: \"%s\"\nExiting without diffing." % compatFailReason)
+        print("Save compatibility check failed.\nReason: \"%s\"\nExiting without diffing." %
+              compatFailReason)
         return
 
     # split lone Gen 4 save
-    if len(fileBuffs) == 1:
-        split = Sav.canSplit(fileBuffs[0])
-        if not split:
-            print("Cannot split a single save file from this game. Exiting without diffing.")
+    if len(saves) == 1:
+        if not saves[0].canSplit():
+            print(
+                "Cannot split a single %s save file. Exiting without diffing." % saves[0].verGroupAbbr)
             return
-        Sav.splitSave(saveFiles, fileBuffs)
+        # split lone save
+        Sav.splitSave(saves, 0, True)
+        config['isSplit'] = True
 
     print("Generating diff...")
-    diff = generateDiff(saveFiles, fileBuffs, params.range, params.e)
+    diff = generateDiff(saves, config)
 
     # output diff
     if diffs == 0:
-        print("Saves were identical.%s" % ("" if params.o == None else " %s not written to." % params.o))
+        print("Saves were identical.%s" %
+              ("" if params.o == None else " %s not written to." % params.o))
     elif params.o != None:
         outDir = ''
         try:
@@ -119,7 +179,8 @@ def main(params):
             os.makedirs(outDir, exist_ok=True)
         except:
             pass
-        outFile = "%s%s" % (params.o, "" if params.o[-4:] == ".txt" else ".txt")
+        outFile = "%s%s" % (
+            params.o, "" if params.o[-4:] == ".txt" else ".txt")
         with open(outFile, 'w') as f:
             f.write(diff)
         print("Diff written to %s successfully" % outFile)
@@ -134,7 +195,11 @@ if __name__ == '__main__':
         if params.saves == None:
             params.saves = []
         for d in range(len(params.dir)):
+            dirFiles = []
             for path, _, files in os.walk(params.dir[d]):
                 for fullname in files:
-                    params.saves.append(os.path.normpath(os.path.join(path, fullname)))
+                    dirFiles.append(os.path.normpath(
+                        os.path.join(path, fullname)))
+            dirFiles.sort(key=str.lower)
+            params.saves.extend(dirFiles)
     main(params)
