63a62f4bbd1c9ecf3ffc2ae4ac19478ccddd47ba
Allen <allenlydiard@gmail.com>
2019-08-08 10:13:12 -0300

    Create sendScript.py

---
+++ sendScript.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import socket, os, sys, struct
+
+
+def send(file, ip):
+    host = ip
+    port = 34567
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(struct.pack('i', os.fstat(file.fileno()).st_size))
+    s.close()
+    input("Press enter to send data")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    data = file.read()
+    s.sendall(data)
+    s.close()
+
+
+
+
+def main(args):
+    filename = ""
+    ip = ""
+    argCount = len(args)
+    
+    if argCount == 1:
+        filename = input("Please enter the filename of the script to send: ")
+        ip = input("Please enter PKSM's IP: ")
+    elif argCount == 2:
+        filename = args[1]
+        ip = input("Please enter PKSM's IP: ")
+    elif argCount == 3:
+        filename = args[1]
+        ip = sys.argv[2]
+    
+    with open(filename,'rb') as f:
+        send(f, ip)
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
