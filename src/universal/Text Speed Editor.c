071fcd0ca897826d1986fd78809a3ad716958e24
SNBeast <snburchett@gmail.com>
2020-06-10 22:09:10 +0000

    Text Speed Editing Script (#23)
    * Instant Text Speed Script for Gen 6
    
    * Update Instant Text Script to Support Gen 7
    
    * Update with Text Speed Options and DP Support
    
    (Thanks again SpiredMoth!)
    
    * Let
    
    * Instant text doesn't exist in DP.
    
    * Revert "Change Applied!"
    
    It may cause some to think that saving is unnecessary, and something that no other scripts do.
    
    * Disable Instant ioption for DP

---
+++ src/universal/Text Speed Editor.c
@@ -0,0 +1,60 @@
+#include <pksm.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    unsigned char version = *argv[0];
+
+    unsigned int configOffset;
+    int noInstant = 0;
+    switch (version)
+    {
+        case 10:
+        case 11:
+            configOffset = 0x60 + sav_gbo();
+            noInstant++;
+            break;
+        case 24:
+        case 25:
+            configOffset = 0x16200;
+            break;
+        case 26:
+        case 27:
+            configOffset = 0x16C00;
+            break;
+        case 30:
+        case 31:
+            configOffset = 0x4400;
+            break;
+        case 32:
+        case 33:
+            configOffset = 0x4800;
+            break;
+        case 42:
+        case 43:
+            configOffset = 0x5600;
+            break;
+        default:
+            gui_warn("Game not supported");
+            return 1;
+    }
+    
+    /*thanks SpiredMoth for the below!*/
+    char *opts[5] = {
+        "Exit Script",
+        "Slow",
+        "Mid",
+        "Fast",
+        "Instant"
+    };
+    int optionCount = 5;
+    if (noInstant) {
+        optionCount--;
+    }
+    int choice = gui_menu_20x2("Pick a text speed option", optionCount, opts);
+    if (choice) {
+        sav_set_byte((sav_get_byte(configOffset, 0) & 0xFC) | (choice - 1), configOffset, 0);
+    }
+
+    return 0;
+}
