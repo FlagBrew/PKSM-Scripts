03b7bca3638a9073693727c27a00fb86a30b96bd
FM1337 <5448220+FM1337@users.noreply.github.com>
2023-01-10 20:02:32 -0400

    Fix PP.

--- src/universal/generate-random-pkmn.c
+++ src/universal/generate-random-pkmn.c
@@ -75,7 +75,19 @@ int main(int argc, char **argv)
     {
         gui_warn("Please enter how many pokemon\nyou would like to generate!");
         gui_numpad(&randomizedCount, "Number of PKMN to generate.", 3);
+
+        maxSlots = sav_get_max(MAX_SLOTS);
+        slotsPerBox = maxSlots / sav_get_max(MAX_BOXES);
+
+        if (randomizedCount > maxSlots || randomizedCount == 0)
+        {
+            char part2[80] = {0};
+            sprintf(part2, "You've inputted an invalid number\nYou must generate between 1-%d pokemon!", maxSlots);
+            gui_warn(part2);
+            return 1;
+        }
     }
+
     int gen_eggs = 0;
 
     if (gen != GEN_ONE)
@@ -91,8 +103,6 @@ int main(int argc, char **argv)
         gui_warn("You must choose a level\nbetween 1-100!");
         return 1;
     }
-    maxSlots = sav_get_max(MAX_SLOTS);
-    slotsPerBox = maxSlots / sav_get_max(MAX_BOXES);
 
     switch (gen)
     {
@@ -155,110 +165,116 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (randomizedCount > maxSlots || randomizedCount == 0)
+    int randNick = gui_choice("Do you want to randomize nicknames?\nThis could produce weirdness.");
+    gui_warn("Depending on how many to generate\nthis might take awhile!");
+    sav_box_decrypt();
+    srand(time(0) + version);
+    char *data = malloc(pkx_box_size(gen));
+    for (int i = 0; i < randomizedCount; i++)
     {
-        char part2[80] = {0};
-        sprintf(part2, "You've inputted an invalid number\nYou must generate between 1-%d pokemon!", maxSlots);
-        gui_warn(part2);
-    }
-    else
-    {
-        int randNick = gui_choice("Do you want to randomize nicknames?\nThis could produce weirdness.");
-        gui_warn("Depending on how many to generate\nthis might take awhile!");
-        sav_box_decrypt();
-        srand(time(0) + version);
-        char *data = malloc(pkx_box_size(gen));
-        for (int i = 0; i < randomizedCount; i++)
+        // Species ID
+        int pokemonID = rand() % maxSpecies + 1;
+        if (gen == GEN_LGPE)
         {
-            // Species ID
-            int pokemonID = rand() % maxSpecies + 1;
-            if (gen == GEN_LGPE)
+            if (pokemonID == 152)
             {
-                if (pokemonID == 152)
-                {
-                    pokemonID = 808;
-                }
-                else if (pokemonID == 153)
-                {
-                    pokemonID = 809;
-                }
+                pokemonID = 808;
             }
-            pkx_generate(data, pokemonID);
-            // Level
-            pkx_set_value(data, gen, LEVEL, rand() % maxLevel + 1);
-            // IVs
-            pkx_set_value(data, gen, IV_HP, rand() % maxIV);
-            pkx_set_value(data, gen, IV_ATK, rand() % maxIV);
-            pkx_set_value(data, gen, IV_DEF, rand() % maxIV);
-            pkx_set_value(data, gen, IV_SPEED, rand() % maxIV);
-            pkx_set_value(data, gen, IV_SPATK, rand() % maxIV);
-            pkx_set_value(data, gen, IV_SPDEF, rand() % maxIV);
-            // Shiny Status
-            pkx_set_value(data, gen, SHINY, rand() % 2);
-            // Nature
-            if (gen != GEN_ONE && gen != GEN_TWO)
+            else if (pokemonID == 153)
             {
-                pkx_set_value(data, gen, NATURE, rand() % 25);
-            }
-            if (gen != GEN_ONE)
-            {
-                // Gender
-                pkx_set_value(data, gen, GENDER, rand() % 3);
-                // Pokerus
-                pkx_set_value(data, gen, POKERUS, rand() % 16, rand() % 5);
-            }
-            // Since LGPE is weird, we can't really do extra stuff for it.
-            if (gen != GEN_LGPE)
-            {
-                for (int ii = 0; ii < 4; ii++)
-                {
-                    // Move
-                    pkx_set_value(data, gen, MOVE, ii, rand() % maxMoves + 1);
-                }
-                // Held Item
-                if (gen != GEN_ONE)
-                {
-                    pkx_set_value(data, gen, ITEM, rand() % maxItems + 1);
-                }
-                if (gen != GEN_ONE && gen != GEN_TWO)
-                {
-                    // Ability
-                    pkx_set_value(data, gen, ABILITY, rand() % maxAbility + 1);
-                    // Capture Ball
-                    pkx_set_value(data, gen, BALL, rand() % maxBalls + 1);
-                }
-            }
-            if (randNick == 1)
-            {
-                // Nickname
-                char *species1 = i18n_species(pokemonID);
-                char *species2 = i18n_species(rand() % maxSpecies + 1);
-
-                int l1 = strlen(species1) / 2;
-                int l2 = strlen(species2) / 2;
-                char *name = malloc(l1 + l2 + 1);
-                memcpy(name, species1, l1);
-                memcpy(name + l1, species2 + l2, strlen(species2) - l2);
-                name[l1 + l2] = '\0';
-                pkx_set_value(data, gen, NICKNAME, name);
-                free(name);
-            }
-            if (gen_eggs == 1)
-            {
-                pkx_set_value(data, gen, EGG, 1);
-            }
-            if (gen_party == 1)
-            {
-                party_inject_pkx(data, gen, i);
-            }
-            else
-            {
-                sav_inject_pkx(data, gen, i / slotsPerBox, i % slotsPerBox, 0);
+                pokemonID = 809;
             }
         }
+        pkx_generate(data, pokemonID);
+        // Level
+        pkx_set_value(data, gen, LEVEL, rand() % maxLevel + 1);
+        // IVs
+        pkx_set_value(data, gen, IV_HP, rand() % maxIV);
+        pkx_set_value(data, gen, IV_ATK, rand() % maxIV);
+        pkx_set_value(data, gen, IV_DEF, rand() % maxIV);
+        pkx_set_value(data, gen, IV_SPEED, rand() % maxIV);
+        pkx_set_value(data, gen, IV_SPATK, rand() % maxIV);
+        pkx_set_value(data, gen, IV_SPDEF, rand() % maxIV);
+        // Shiny Status
+        pkx_set_value(data, gen, SHINY, rand() % 2);
+        // Nature
+        if (gen != GEN_ONE && gen != GEN_TWO)
+        {
+            pkx_set_value(data, gen, NATURE, rand() % 25);
+        }
+        if (gen != GEN_ONE)
+        {
+            // Gender
+            pkx_set_value(data, gen, GENDER, rand() % 3);
+            // Pokerus
+            pkx_set_value(data, gen, POKERUS, rand() % 16, rand() % 5);
+        }
+        // Since LGPE is weird, we can't really do extra stuff for it.
+        if (gen != GEN_LGPE)
+        {
+            for (int ii = 0; ii < 4; ii++)
+            {
+                int moveNum = rand() % maxMoves + 1;
+                while (!sav_check_value(SAV_VALUE_MOVE, moveNum))
+                {
+                    moveNum = rand() % maxMoves + 1;
+                }
+                // Move
+                pkx_set_value(data, gen, MOVE, ii, moveNum);
 
-        free(data);
-        sav_box_encrypt();
+                // PP
+                int ppUps = rand() % 3 + 1;
+                pkx_set_value(data, gen, PP_UPS, ii, ppUps);
+                pkx_set_value(data, gen, PP, ii, (rand() % max_pp(gen, moveNum, ppUps)) + 1);
+            }
+            // Held Item
+            if (gen != GEN_ONE)
+            {
+                int itemNum = rand() % maxItems + 1;
+                while (!sav_check_value(SAV_VALUE_ITEM, itemNum))
+                {
+                    itemNum = rand() % maxItems + 1;
+                }
+                pkx_set_value(data, gen, ITEM, itemNum);
+            }
+            if (gen != GEN_ONE && gen != GEN_TWO)
+            {
+                // Ability
+                pkx_set_value(data, gen, ABILITY, rand() % maxAbility + 1);
+                // Capture Ball
+                pkx_set_value(data, gen, BALL, rand() % maxBalls + 1);
+            }
+        }
+        if (randNick == 1)
+        {
+            // Nickname
+            char *species1 = i18n_species(pokemonID);
+            char *species2 = i18n_species(rand() % maxSpecies + 1);
+
+            int l1 = strlen(species1) / 2;
+            int l2 = strlen(species2) / 2;
+            char *name = malloc(l1 + l2 + 1);
+            memcpy(name, species1, l1);
+            memcpy(name + l1, species2 + l2, strlen(species2) - l2);
+            name[l1 + l2] = '\0';
+            pkx_set_value(data, gen, NICKNAME, name);
+            free(name);
+        }
+        if (gen_eggs == 1)
+        {
+            pkx_set_value(data, gen, EGG, 1);
+        }
+        if (gen_party == 1)
+        {
+            party_inject_pkx(data, gen, i);
+        }
+        else
+        {
+            sav_inject_pkx(data, gen, i / slotsPerBox, i % slotsPerBox, 0);
+        }
     }
+
+    free(data);
+    sav_box_encrypt();
     return 0;
 }
